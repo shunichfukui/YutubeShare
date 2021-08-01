99e8362eb094f56da2d69b3f82c6988ae1abcb57
shunichfukui <68207981+shunichfukui@users.noreply.github.com>
2021-08-01 22:45:24 +0900

    Alert処理

--- Yutube App 1/Controller/ProfileViewController.swift
+++ Yutube App 1/Controller/ProfileViewController.swift
@@ -9,14 +9,14 @@ import UIKit
 import Photos
 import FirebaseFirestore
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     var userName = String()
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var button: UIButton!
 
-    override func viewDidLoad() {
+    override func viewDidxLoad() {
         super.viewDidLoad()
         imageView.layer.cornerRadius = 10
         textView.layer.cornerRadius = 10
@@ -24,6 +24,18 @@ class ProfileViewController: UIViewController {
         checkCamera()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func tap(_ sender: Any) {
+        showAlert()
+        
+    }
+    // タッチされたときに呼ばれる場所
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // キーボードを閉じる
+        textView.resignFirstResponder()
+        
+    }
+
     func checkCamera() {
         // ユーザーに許可を促す.
         PHPhotoLibrary.requestAuthorization { (status) -> Void in
@@ -31,12 +43,56 @@ class ProfileViewController: UIViewController {
             case .authorized:print("Authorized")
             case .denied:print("NotDetermined")
             case .restricted: print("limited")
+            case .notDetermined:
+                <#code#>
+            case .limited:
+                <#code#>
             @unknown default: break
             }
             <#code#>
         }
     }
     
+    func showAlert(){
+        let alert:UIAlertController = UIAlertController(title:"選択してください。", message: "カメラアルバムをどちらにしますか？", preferredStyle: .alert)
+        // カメラボタン
+        let cameraAction: UIAlertAction = UIAlertAction(title: "カメラ", style: .default, handler: {(action: UIAlertAction!)-> Void in self.createImagePicker(sourceType: .camera)
+        })
+        // アルバムボタン
+        let albumAction: UIAlertAction = UIAlertAction(title: "アルバム", style: .default, handler: {
+            // ボタンが押された時の処理
+            (action:UIAlertAction!)-> Void in
+            self.createImagePicker(sourceType: .photoLibrary)
+        })
+        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: .cancel)
+        
+        alert.addAction(albumAction)
+        alert.addAction(cameraAction)
+        alert.addAction(cancelAction)
+        
+        // Alertを表示
+        present(alert, animated: true, completion: nil)
+    }
+
+    func createImagePicker(sourceType: UIImagePickerController.SourceType){
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.sourceType = sourceType
+        cameraPicker.delegate = self
+        cameraPicker.allowsEditing = true
+        self.present(cameraPicker, animated: true, completion: nil)
+    }
+
+    // 撮影がキャンセルになった時に呼ばれる
+    func imagePickerControllerDidCancel (_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey: Any]){
+        if let pickedImage = info[.editedImage] as? UIImage {
+            imageView.image = pickedImage
+            // 閉じる処理
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
 
     /*
     // MARK: - Navigation
